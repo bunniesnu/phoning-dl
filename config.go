40d9f5d680b55d7718fdeb6b65c331fb09c7d704
Bunniesnu <bunniesnu@gmail.com>
2025-07-27 01:42:33 +0900

    Rename app.go -> config.go

--- config.go
+++ config.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"path"
-
-	"fyne.io/fyne/v2"
 )
 
 func (m *App) LoadConfig() error {
@@ -45,19 +42,4 @@ func (m *App) SaveConfig() error {
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ")
 	return encoder.Encode(m.config)
-}
-
-func (m *App) UpdateState(state int) error {
-	if m.w == nil {
-		return fmt.Errorf("window is nil")
-	}
-	var screen *fyne.Container
-	switch state {
-	case 0: // Load config
-		screen = m.LoadingConfigScreen()
-	default:
-		return fmt.Errorf("unknown state: %d", state)
-	}
-	(*m.w).SetContent(screen)
-	return nil
 }
