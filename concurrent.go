86d608a93b34fb19c1540174e414ec8dd12700b7
Bunniesnu <bunniesnu@gmail.com>
2025-07-31 18:45:55 +0900

    Add concurrentExecuteAnyWithContext

--- concurrent.go
+++ concurrent.go
@@ -92,3 +92,44 @@ func concurrentExecuteAny[T any, R any](f func(T, context.Context) (R, error), i
 			return results, nil
 	}
 }
+
+func concurrentExecuteAnyWithContext[T any, R any](f func(T, context.Context) (R, error), items []T, concurrency int, ctx context.Context) ([]R, error) {
+	var wg sync.WaitGroup
+	results := make([]R, 0, len(items))
+	var mu sync.Mutex
+	sem := make(chan struct{}, concurrency) // limit concurrent executions
+	// Use context to handle cancellation on error
+	errCh := make(chan error, 1)
+	for _, item := range items {
+		wg.Add(1)
+		go func(param T){
+			sem <- struct{}{} // acquire a slot
+			defer func() { <-sem }() // release the slot
+			defer wg.Done()
+			select {
+				case <-ctx.Done():
+					return
+				default:
+			}
+			res, err := f(param, ctx)
+			if err != nil {
+				errCh <- err
+				return
+			}
+			mu.Lock()
+			results = append(results, res)
+			mu.Unlock()
+		}(item)
+	}
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+	select {
+		case err := <-errCh:
+			return nil, err
+		case <-done:
+			return results, nil
+	}
+}
